6bea9f23bae117cd07868a129561bd6fb991748e
61-Mime <jfmpsantos@gmail.com>
2021-03-06 17:28:59 +0000

    correr em WIN32

--- generator/plane.cpp
+++ generator/plane.cpp
@@ -3,5 +3,13 @@
 Shape* plane(float size) {
     Shape *s = new Shape();
 
+    s->addPoint(size/2, 0.0f, size/2);
+    s->addPoint(-size/2, 0.0f, -size/2);
+    s->addPoint(-size/2, 0.0f, size/2);
+
+    s->addPoint(size / 2, 0.0f, size / 2);
+    s->addPoint(size / 2, 0.0f, -size / 2);
+    s->addPoint(-size / 2, 0.0f, -size / 2);
+
     return s;
 }
