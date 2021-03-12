e82ca65974c9ad14a05888d29e441b2f4f77dc5a
61-Mime <jfmpsantos@gmail.com>
2021-03-12 17:29:32 +0000

    esfera concluida

--- generator/sphere.cpp
+++ generator/sphere.cpp
@@ -6,50 +6,28 @@
 Shape* sphere(float radius, int slices, int stacks) {
     Shape* s = new Shape();
 
-    float angleSt = M_PI / stacks;
-    float angleSl = 2 * M_PI / slices;
+	float angleSt = M_PI / stacks;
+	float angleSl = (2 * M_PI) / slices;
 
-    float alfa, b, beta_in, beta_out;
+	float rectAngle = (M_PI / 2);
+	float beta;
+	float alpha;
 
-    for (int j = 0; j < stacks; j++) {
-       
-        b = 0.01 * j;
-        alfa = j * (2 * M_PI / slices);
+	for (int j = 0; j < stacks; j++) {
+		beta = j * angleSt - rectAngle;
 
-        beta_in = M_PI / 2;
-        beta_out = beta_in - angleSt;
+		for (int i = 0; i < slices; i++) {
+			alpha = i * angleSl;
 
-        for (int i = j; i < slices; i++) {
+			s->addPoint(new Point(radius * cos(alpha) * cos(beta), radius * sin(beta), radius * sin(alpha) * cos(beta)));
+			s->addPoint(new Point(radius * cos(alpha) * cos(beta + angleSt), radius * sin(beta + angleSt), radius * sin(alpha) * cos(beta + angleSt)));
+			s->addPoint(new Point(radius * cos(alpha + angleSl) * cos(beta), radius * sin(beta), radius * sin(alpha + angleSl) * cos(beta)));
 
-            if (i == 1) {
-                s->addPoint(new Point(radius * cos(alfa) * cos(beta_out), radius * sin(beta_out), radius * sin(alfa) * cos(beta_out)));
-                s->addPoint(new Point(0, radius, 0));
-                s->addPoint(new Point(radius * cos(alfa + angleSl) * cos(beta_out), radius * sin(beta_out), radius * sin(alfa + angleSl) * cos(beta_out)));
-                beta_in -= angleSt;
-                beta_out -= angleSt;
-            }
-            else if (i == slices) {
-                s->addPoint(new Point(0, -radius, 0));
-                s->addPoint(new Point(radius * cos(alfa) * cos(beta_in), radius * sin(beta_in), radius * sin(alfa) * cos(beta_in)));
-                s->addPoint(new Point(radius * cos(alfa + angleSl) * cos(beta_in), radius * sin(beta_in), radius * sin(alfa + angleSl) * cos(beta_in)));
-                beta_in -= angleSt;
-                beta_out -= angleSt;
-            }
-            else {
-                s->addPoint(new Point(radius * cos(alfa) * cos(beta_in), radius * sin(beta_in), radius * sin(alfa) * cos(beta_in)));
-                s->addPoint(new Point(radius * cos(alfa + angleSl) * cos(beta_in), radius * sin(beta_in), radius * sin(alfa + angleSl) * cos(beta_in)));
-                s->addPoint(new Point(radius * cos(alfa + angleSl) * cos(beta_out), radius * sin(beta_out), radius * sin(alfa + angleSl) * cos(beta_out)));
-
-                s->addPoint(new Point(radius * cos(alfa) * cos(beta_out), radius * sin(beta_out), radius * sin(alfa) * cos(beta_out)));
-                s->addPoint(new Point(radius * cos(alfa) * cos(beta_in), radius * sin(beta_in), radius * sin(alfa) * cos(beta_in)));
-                s->addPoint(new Point(radius * cos(alfa + angleSl) * cos(beta_out), radius * sin(beta_out), radius * sin(alfa + angleSl) * cos(beta_out)));
-
-                beta_in -= angleSt;
-                beta_out -= angleSt;
-            }
-        }
-
-    }
+			s->addPoint(new Point(radius * cos(alpha) * cos(beta + angleSt), radius * sin(beta + angleSt), radius * sin(alpha) * cos(beta + angleSt)));
+			s->addPoint(new Point(radius * cos(alpha + angleSl) * cos(beta + angleSt), radius * sin(beta + angleSt), radius * sin(alpha + angleSl) * cos(beta + angleSt)));
+			s->addPoint(new Point(radius * cos(alpha + angleSl) * cos(beta), radius * sin(beta), radius * sin(alpha + angleSl) * cos(beta)));
+		}
+	}
 
     return s;
 }
