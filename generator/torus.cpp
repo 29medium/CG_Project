48a5263f142bfa7ed0f5eae7927c0aaad11e6b6c
61-Mime <a89520@alunos.uminho.pt>
2021-03-29 17:36:41 +0100

    Torus

---
+++ generator/torus.cpp
@@ -0,0 +1,50 @@
+include "torus.h"
+
+Point pointCoord(float radiusIn, float radiusOut, float alpha, float beta) {
+
+  Point point;
+
+  point = new Point((radiusOut + radiusIn * (cos(beta))) * cos(alpha),
+                    (radiusOut + radiusIn * (cos(beta))) * sin(alpha),
+                    radiusIn * sin(beta));
+
+  return point;
+}
+
+Shape* drawRetangle(float radiusIn, float radiusOut, float alpha, float beta, float nextAlpha,float nextBeta, Shape* s) {
+
+  s->addPoint(pointCoord(radiusIn, radiusOut, alpha, nextBeta));
+  s->addPoint(pointCoord(radiusIn, radiusOut, alpha, beta));
+  s->addPoint(pointCoord(radiusIn, radiusOut, nextAlpha, nextBeta));
+
+  s->addPoint(pointCoord(radiusIn, radiusOut, nextAlpha, nextBeta));
+  s->addPoint(pointCoord(radiusIn, radiusOut, alpha, beta));
+  s->addPoint(pointCoord(radiusIn, radiusOut, nextAlpha, beta));
+
+  return s;
+}
+
+Shape* torus(float radiusIn,float radiusOut , int slices, int layers)
+{
+        Shape* s = new Shape();
+        float alpha, nextAlpha,
+                beta, nextBeta;
+				float sliceAngle = 2 * M_PI / slices;
+				float layerAngle = 2 * M_PI / layers;
+
+        for (int i = 0; i < layers; i++)
+        {
+                beta = i * layerAngle;
+                nextBeta = (i + 1) * layerAngle;
+
+                for (int j = 0; j < slices; j++)
+                {
+                        alpha = j * sliceAngle;
+                        nextAlpha = (j + 1) * sliceAngle;
+
+                        drawRetangle(radiusIn, radiusOut, alpha, beta, nextAlpha, nextBeta, s);
+                }
+        }
+
+        return s;
+}
---
+++ generator/torus.h
@@ -0,0 +1,9 @@
+#ifndef GENERATOR_THORUS_H
+#define GENERATOR_THORUS_H
+
+#include "../src/shape.h"
+#include "../src/point.h"
+
+Shape* torus(float radiusIn, float radiusOut, int slice, int layers);
+
+#endif //GENERATOR_THORUS_H
