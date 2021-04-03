179734834143625f1c823672070c8fe2f1a3a728
Paulo Sousa <45037062+29medium@users.noreply.github.com>
2021-04-03 21:21:30 +0100

    Camera speed

--- engine/camera.cpp
+++ engine/camera.cpp
@@ -66,7 +66,7 @@ float Camera::getCameraUpZ()
 
 void Camera::setCameraSpeed(float cameraSpeed)
 {
-    this->cameraSpeed = cameraSpeed * speedMultiplier * 10;
+    this->cameraSpeed = cameraSpeed * speedMultiplier * 50;
 }
 
 Point *Camera::multiplyVectorBySpeed(Point *p) const
