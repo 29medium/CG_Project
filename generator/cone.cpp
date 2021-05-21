12c48a5e0aad1f1aa9308519f7fc95d90c74efef
luissobral4 <luisfilipesobral@gmail.com>
2021-05-21 13:02:14 +0100

    normal&texture

--- generator/cone.cpp
+++ generator/cone.cpp
@@ -3,6 +3,20 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+Point * getNormalCone(float edge [3], Point * p, float tangAlpha){
+    float point_aux[3];
+
+    p->get(point_aux);
+    float distance = dist_2_P(edge,point_aux);
+
+    float height = quadratic_equation(distance*tangAlpha,point_aux);
+
+    float res [3];
+    float point_at_aixis [3] = {0.0,height,0.0};
+    vector_between_normalized(point_at_aixis,point_aux,res);
+    return new Point(res[0],res[1],res[2]);
+}
+
 void drawBase(float radius, float height, int slices, int stacks, float sliceAngle, Shape* s) {
     int i;
     float alpha;
@@ -26,18 +40,42 @@ void drawSide(float radius, float height, int slices, int stacks, float sliceAng
     float height_next = height / stacks;
     float radius_now = radius;
     float radius_next = radius - (radius / stacks);
+    float edge [3] = {0.0, height, 0.0};
+    float tangAlpha = radius / height;
+
+    Point *p1,*p2,*p3;
 
     for (i = 0; i < slices; i++) {
         alpha = i * sliceAngle;
         for (k = 0; k < stacks; k++) {
+            p1 = new Point(radius_now * cos(alpha + sliceAngle), height_now, radius_now * sin(alpha + sliceAngle));
+            p2 = new Point(radius_now * cos(alpha), height_now, radius_now * sin(alpha)));
+            p3 = new Point(radius_next * cos(alpha + sliceAngle), height_next, radius_next * sin(alpha + sliceAngle)));
 
-            s->addPoint(new Point(radius_now * cos(alpha + sliceAngle), height_now, radius_now * sin(alpha + sliceAngle)));
-            s->addPoint(new Point(radius_now * cos(alpha), height_now, radius_now * sin(alpha)));
-            s->addPoint(new Point(radius_next * cos(alpha + sliceAngle), height_next, radius_next * sin(alpha + sliceAngle)));
+            s->addPoint(p1);
+            s->addNormal(getNormalCone(edge,p1,tangAlpha));
 
-            s->addPoint(new Point(radius_now * cos(alpha), height_now, radius_now * sin(alpha)));
-            s->addPoint(new Point(radius_next * cos(alpha), height_next, radius_next * sin(alpha)));
-            s->addPoint(new Point(radius_next * cos(alpha + sliceAngle), height_next, radius_next * sin(alpha + sliceAngle)));
+            s->addPoint(p2);
+            s->addNormal(getNormalCone(edge,p1,tangAlpha));
+
+
+            s->addPoint(p3);
+            s->addNormal(getNormalCone(edge,p1,tangAlpha));
+
+
+            p1 = new Point(radius_now * cos(alpha), height_now, radius_now * sin(alpha)));
+            p2 = new Point(radius_next * cos(alpha), height_next, radius_next * sin(alpha)));
+            p3 = new Point(radius_next * cos(alpha + sliceAngle), height_next, radius_next * sin(alpha + sliceAngle)));
+            
+            s->addPoint(p1);
+            s->addNormal(getNormalCone(edge,p1,tangAlpha));
+
+            s->addPoint(p2);
+            s->addNormal(getNormalCone(edge,p1,tangAlpha));
+
+
+            s->addPoint(p3);
+            s->addNormal(getNormalCone(edge,p1,tangAlpha));
 
             height_now = height_next;
             height_next += height / stacks;
